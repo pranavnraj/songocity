6049e125bc971c166f6a42ef413ced791b33269a
Pranav <pranavnraj@gmail.com>
2020-12-23 17:48:10 -0800

    Init web server

---
+++ src/main/java/com/spotifywebapp/app/GreetingController.java
@@ -0,0 +1,22 @@
+package com.spotifywebapp.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
+
