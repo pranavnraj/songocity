85962f7d7fa24a7571deec50dc7d2c2aa7d454e4
pranavnraj <pranavnraj@gmail.com>
2020-12-27 21:17:12 -0800

    Init Auth Login Flow

---
+++ src/main/java/com/spotifywebapp/app/LoginCredentialConstants.java
@@ -0,0 +1,8 @@
+package com.spotifywebapp.app;
+
+public class LoginCredentialConstants {
+
+    public static final String CLIENT_ID = "db69666057c14f1795515121f961890e";
+    public static final String CLIENT_SECRET = "d9c6066f4eb447d8bedecb3a4eaf5fce";
+    public static final String REDIRECT_URI = "http://localhost:8888/callback/";
+}
