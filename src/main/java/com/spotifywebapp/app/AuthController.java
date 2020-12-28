fcf7b73d774000a19793fb8d3c6316d3be3c6c86
pranavnraj <pranavnraj@gmail.com>
2020-12-27 21:17:12 -0800

    Init Auth Login Flow

--- src/main/java/com/spotifywebapp/app/AuthController.java
+++ src/main/java/com/spotifywebapp/app/AuthController.java
@@ -6,15 +6,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.spotifywebapp.app.SpotifyWebAPI;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+
 @Controller
-public class GreetingController {
+public class AuthController {
+
+    public SpotifyWebAPI api;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/login")
+    public void login() {
+        api = new SpotifyWebAPI();
+        api.authorizeAPI();
+    }
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code) {
+        api.setAuthCode(code);
+        api.refreshTokenAPI();
+        api.accessTokenAPI();
+        HashMap<String, String> userInfo = api.currentUserAPI();
+
+        userInfo.get("display_name");
         return "greeting";
     }
 
