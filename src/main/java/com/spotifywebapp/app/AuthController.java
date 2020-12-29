f2e8612d793d11b5f01380e453dfae963ded3063
Ayush Arora <ayusharora99@yahoo.com>
2020-12-28 19:51:55 -0800

    Current User API calls

--- src/main/java/com/spotifywebapp/app/AuthController.java
+++ src/main/java/com/spotifywebapp/app/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import com.spotifywebapp.app.SpotifyWebAPI;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Controller
@@ -33,9 +35,58 @@ public class AuthController {
         api.setAuthCode(code);
         api.refreshTokenAPI();
         api.accessTokenAPI();
+
         HashMap<String, String> userInfo = api.currentUserAPI();
 
-        System.out.println(userInfo.get("display_name"));
+        // Profile
+        System.out.println("Display Name: " + userInfo.get("display_name"));
+        System.out.println();
+        ArrayList<String> userRecentTracks = api.currentUserRecentTracks();
+        System.out.println("Recently Listened to: " + userRecentTracks);
+        System.out.println();
+        ArrayList<String> userTopArtists = api.currentUserTopArtists();
+        System.out.println("Your Top Artists... " + userTopArtists);
+        System.out.println();
+        ArrayList<String> userTopTracks = api.currentUserTopTracks();
+        System.out.println("Your Top Tracks... " + userTopTracks);
+        System.out.println();
+
+
+        // Creating Hashmap to be passed to python
+        System.out.println("Analyzing Playlists... ");
+        HashMap<String, HashMap<String, HashMap<String, Float>>> playlistsInfo = new HashMap<String, HashMap<String, HashMap<String, Float>>>();
+        HashMap<String, String> playlistInfo = api.currentUserPlaylists();
+        for(String playlistId: playlistInfo.keySet() ){
+            String playlistName = playlistInfo.get(playlistId);
+            playlistsInfo.put(playlistName, new HashMap<String, HashMap<String, Float>>());
+
+            System.out.println(playlistName + ": " + playlistId);
+            HashMap<String,String> playlistTracks = api.getTracks(playlistId);
+            HashMap<String,HashMap<String,Float>> playlistTrackInfo = api.getTracksInfo(playlistTracks);
+            for(String trackId: playlistTrackInfo.keySet()){
+                String trackName = playlistTracks.get(trackId);
+//                System.out.println("Analyzing " +  trackName + " ...");
+                playlistsInfo.get(playlistName).put(trackName, new HashMap<String, Float>());
+                for(String feature : playlistTrackInfo.get(trackId).keySet()){
+                    playlistsInfo.get(playlistName).get(trackName).put(feature, playlistTrackInfo.get(trackId).get(feature));
+//                    System.out.println(feature + ": " + playlistTrackInfo.get(trackId).get(feature));
+                }
+            }
+
+        }
+
+        System.out.println("The Hashmap to be passed to Python");
+
+        for(String playlistName : playlistsInfo.keySet()){
+            System.out.println("playlist: " + playlistName);
+            for(String trackName : playlistsInfo.get(playlistName).keySet()){
+                System.out.println("track: " + trackName);
+                for(String feature: playlistsInfo.get(playlistName).get(trackName).keySet()){
+                    System.out.println(feature + ": " + playlistsInfo.get(playlistName).get(trackName).get(feature));
+                }
+            }
+        }
+
         return "homepage";
     }
 
--- src/main/java/com/spotifywebapp/app/SpotifyWebAPI.java
+++ src/main/java/com/spotifywebapp/app/SpotifyWebAPI.java
@@ -12,13 +12,24 @@ import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 
+import com.wrapper.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
+import com.wrapper.spotify.requests.data.personalization.simplified.GetUsersTopTracksRequest;
+import com.wrapper.spotify.requests.data.player.GetCurrentUsersRecentlyPlayedTracksRequest;
+import com.wrapper.spotify.requests.data.playlists.GetListOfCurrentUsersPlaylistsRequest;
+import com.wrapper.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
+import com.wrapper.spotify.requests.data.tracks.GetAudioFeaturesForSeveralTracksRequest;
+import com.wrapper.spotify.requests.data.tracks.GetAudioFeaturesForTrackRequest;
 import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 import org.apache.hc.core5.http.ParseException;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletionException;
 
 import com.spotifywebapp.app.LoginCredentialConstants;
 
@@ -41,7 +52,7 @@ public class SpotifyWebAPI {
     }
 
     public void authorizeAPI() {
-        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().build();
+        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().scope("user-top-read,user-read-recently-played").build();
         URI authorizeURI = authorizationCodeUriRequest.execute();
         Runtime rt = Runtime.getRuntime();
         try {
@@ -71,6 +82,139 @@ public class SpotifyWebAPI {
         }
 
         return userInfo;
+    }
+
+    public ArrayList<String> currentUserTopArtists(){
+        this.setAccessSpotifyApi();
+
+        GetUsersTopArtistsRequest getUsersTopArtistsRequest = spotifyApi.getUsersTopArtists().build();
+        ArrayList<String> artists = new ArrayList<String>();
+        try{
+            Paging<Artist> artistPaging = getUsersTopArtistsRequest.execute();
+
+            Artist[] items = artistPaging.getItems();
+
+            for(Artist artist: items) {
+                artists.add(artist.getName());
+            }
+        }catch(IOException | SpotifyWebApiException | ParseException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return artists;
+    }
+
+    public ArrayList<String> currentUserTopTracks(){
+        this.setAccessSpotifyApi();
+
+        GetUsersTopTracksRequest getUsersTopTracksRequest = spotifyApi.getUsersTopTracks().build();
+        ArrayList<String> tracks = new ArrayList<String>();
+        try{
+            Paging<Track> trackPaging = getUsersTopTracksRequest.execute();
+
+            Track[] items = trackPaging.getItems();
+
+            for(Track track : items){
+                tracks.add(track.getName());
+            }
+
+        }catch(IOException | SpotifyWebApiException | ParseException e){
+            System.out.print("Error: " + e.getMessage());
+        }
+
+        return tracks;
+    }
+
+    public ArrayList<String> currentUserRecentTracks(){
+        this.setAccessSpotifyApi();
+
+        GetCurrentUsersRecentlyPlayedTracksRequest getCurrentUsersRecentlyPlayedTracksRequest = spotifyApi.getCurrentUsersRecentlyPlayedTracks().build();
+        ArrayList<String> recentTracks = new ArrayList<String>();
+        try{
+            PagingCursorbased<PlayHistory> playHistoryPagingCursorbased = getCurrentUsersRecentlyPlayedTracksRequest.execute();
+
+            PlayHistory[] recentItems = playHistoryPagingCursorbased.getItems();
+
+            for(PlayHistory track : recentItems){
+                recentTracks.add(track.getTrack().getName());
+            }
+
+        }catch(IOException | SpotifyWebApiException | ParseException e){
+            System.out.print("Error: " + e.getMessage());
+        }
+
+        return recentTracks;
+    }
+
+    public HashMap<String,String> currentUserPlaylists(){
+        this.setAccessSpotifyApi();
+
+        GetListOfCurrentUsersPlaylistsRequest getListOfCurrentUsersPlaylistsRequest = spotifyApi.getListOfCurrentUsersPlaylists().build();
+        HashMap<String, String> playlistInfo = new HashMap<String, String>();
+        try{
+            Paging <PlaylistSimplified> playlistSimplifiedPaging = getListOfCurrentUsersPlaylistsRequest.execute();
+
+            PlaylistSimplified[] items = playlistSimplifiedPaging.getItems();
+
+            for(PlaylistSimplified playlist: items){
+                playlistInfo.put(playlist.getId(),playlist.getName());
+            }
+        }catch(IOException | SpotifyWebApiException | ParseException e) {
+            System.out.print("Error: " + e.getMessage());
+        }
+
+        return playlistInfo;
+    }
+
+    public HashMap<String,String> getTracks(String playlistId){
+        GetPlaylistsItemsRequest getPlaylistsItemsRequest = spotifyApi.getPlaylistsItems(playlistId).build();
+        HashMap<String,String> playistTracks = new HashMap<String,String>();
+        try{
+            Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsItemsRequest.execute();
+
+            PlaylistTrack[] items = playlistTrackPaging.getItems();
+
+            for(PlaylistTrack track : items){
+                playistTracks.put(track.getTrack().getId(),track.getTrack().getName());
+            }
+
+        }catch(IOException | SpotifyWebApiException | ParseException e) {
+            System.out.print("Error: " + e.getMessage());
+        }
+
+        return playistTracks;
+    }
+
+    public HashMap<String, HashMap<String, Float>> getTracksInfo(HashMap<String,String> playlistTracks) {
+        //this.setAccessSpotifyApi();
+
+        String[] ids = playlistTracks.keySet().toArray(new String[0]);
+        GetAudioFeaturesForSeveralTracksRequest getAudioFeaturesForSeveralTracksRequest = spotifyApi.getAudioFeaturesForSeveralTracks(ids).build();
+        HashMap<String,HashMap<String,Float>> tracksInfo = new HashMap<String,HashMap<String,Float>>();
+        try {
+            AudioFeatures[] audioFeatures = getAudioFeaturesForSeveralTracksRequest.execute();
+
+            for (AudioFeatures trackInfo : audioFeatures) {
+                if(trackInfo != null) {
+                    tracksInfo.put(trackInfo.getId(), new HashMap<String, Float>());
+                    tracksInfo.get(trackInfo.getId()).put("acousticness", trackInfo.getAcousticness());
+                    tracksInfo.get(trackInfo.getId()).put("danceability", trackInfo.getDanceability());
+                    tracksInfo.get(trackInfo.getId()).put("duration", trackInfo.getDanceability());
+                    tracksInfo.get(trackInfo.getId()).put("energy", trackInfo.getEnergy());
+                    tracksInfo.get(trackInfo.getId()).put("instrumentalness", trackInfo.getInstrumentalness());
+                    tracksInfo.get(trackInfo.getId()).put("mainKey", trackInfo.getKey().floatValue());
+                    tracksInfo.get(trackInfo.getId()).put("liveness", trackInfo.getLiveness());
+                    tracksInfo.get(trackInfo.getId()).put("loudness", trackInfo.getLoudness());
+                    tracksInfo.get(trackInfo.getId()).put("tempo", trackInfo.getTempo());
+                    tracksInfo.get(trackInfo.getId()).put("timeSignature", trackInfo.getTimeSignature().floatValue());
+                    tracksInfo.get(trackInfo.getId()).put("valence", trackInfo.getValence());
+                }
+            }
+        }catch(IOException | SpotifyWebApiException | ParseException e) {
+            System.out.print("Error: " + e.getMessage());
+        }
+        return tracksInfo;
     }
 
     public void refreshTokenAPI() {
@@ -104,6 +248,7 @@ public class SpotifyWebAPI {
         }
 
         this.accessToken = spotifyApi.getAccessToken();
+
 
     }
 
