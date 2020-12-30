a85fd7d9cfc0e46f93a796ce0aed70c4ce166ecb
Ayush Arora <ayusharora99@yahoo.com>
2020-12-29 23:53:29 -0800

    API Calls for Training Data

--- src/main/java/com/spotifywebapp/app/AuthController.java
+++ src/main/java/com/spotifywebapp/app/AuthController.java
@@ -1,5 +1,8 @@
 package com.spotifywebapp.app;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import com.spotifywebapp.app.SpotifyWebAPI;
 import com.spotifywebapp.app.SpotifyWebAPISingleton;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Array;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
@@ -33,7 +39,7 @@ public class AuthController {
     }
 
     @GetMapping("/callback")
-    public @ResponseBody String callback(@RequestParam(name="code") String code) {
+    public @ResponseBody String callback(@RequestParam(name="code") String code) throws IOException {
         api.setAuthCode(code);
         api.refreshTokenAPI();
         api.accessTokenAPI();
@@ -55,22 +61,23 @@ public class AuthController {
 
 
         // Creating Hashmap to be passed to python
+        int numTracks = 0;
         System.out.println("Analyzing Playlists... ");
         HashMap<String, HashMap<String, HashMap<String, Float>>> playlistsInfo = new HashMap<String, HashMap<String, HashMap<String, Float>>>();
         HashMap<String, String> playlistInfo = api.currentUserPlaylists();
         for(String playlistId: playlistInfo.keySet() ){
             String playlistName = playlistInfo.get(playlistId);
-            playlistsInfo.put(playlistName, new HashMap<String, HashMap<String, Float>>());
+            playlistsInfo.put(playlistId, new HashMap<String, HashMap<String, Float>>());
 
             System.out.println(playlistName + ": " + playlistId);
             HashMap<String,String> playlistTracks = api.getTracks(playlistId);
+            numTracks += playlistTracks.keySet().size();
             HashMap<String,HashMap<String,Float>> playlistTrackInfo = api.getTracksInfo(playlistTracks);
             for(String trackId: playlistTrackInfo.keySet()){
                 String trackName = playlistTracks.get(trackId);
-//                System.out.println("Analyzing " +  trackName + " ...");
-                playlistsInfo.get(playlistName).put(trackName, new HashMap<String, Float>());
+                playlistsInfo.get(playlistId).put(trackId, new HashMap<String, Float>());
                 for(String feature : playlistTrackInfo.get(trackId).keySet()){
-                    playlistsInfo.get(playlistName).get(trackName).put(feature, playlistTrackInfo.get(trackId).get(feature));
+                    playlistsInfo.get(playlistId).get(trackId).put(feature, playlistTrackInfo.get(trackId).get(feature));
 //                    System.out.println(feature + ": " + playlistTrackInfo.get(trackId).get(feature));
                 }
             }
@@ -79,15 +86,30 @@ public class AuthController {
 
         System.out.println("The Hashmap to be passed to Python");
 
-        for(String playlistName : playlistsInfo.keySet()){
-            System.out.println("playlist: " + playlistName);
-            for(String trackName : playlistsInfo.get(playlistName).keySet()){
-                System.out.println("track: " + trackName);
-                for(String feature: playlistsInfo.get(playlistName).get(trackName).keySet()){
-                    System.out.println(feature + ": " + playlistsInfo.get(playlistName).get(trackName).get(feature));
-                }
+        for(String playlistName : playlistsInfo.keySet()) {
+//            System.out.println("playlist: " + playlistName);
+            for (String trackName : playlistsInfo.get(playlistName).keySet()) {
+//                System.out.println("track: " + trackName);
+//                for(String feature: playlistsInfo.get(playlistName).get(trackName).keySet()){
+//                    System.out.println(feature + ": " + playlistsInfo.get(playlistName).get(trackName).get(feature));
+//                }
             }
         }
+
+        Gson gson = new GsonBuilder().create();
+        JsonObject ayush = gson.toJsonTree(playlistsInfo).getAsJsonObject();
+        PrintWriter out = new PrintWriter("ayush.txt");
+        out.println(ayush);
+        out.flush();
+
+        HashMap<String,String> genres = api.getReccomendations(numTracks/126);
+        HashMap<String,HashMap<String,Float>> genreInfo = api.getTracksInfo(genres);
+
+        Gson rec_gson = new GsonBuilder().create();
+        JsonObject genreJson = rec_gson.toJsonTree(genreInfo).getAsJsonObject();
+        PrintWriter outRec = new PrintWriter("genres.txt");
+        outRec.println(genreJson);
+        outRec.flush();
 
         HttpClientHandler handler = new HttpClientHandler();
         handler.formRequest("http://localhost:8888/greeting");
