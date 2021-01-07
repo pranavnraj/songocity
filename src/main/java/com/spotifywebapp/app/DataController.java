244b3ff8203e16ba75ec58615bf2577a9a7f9370
pranavnraj <pranavnraj@gmail.com>
2021-01-07 01:09:26 -0800

    Basic Friend Operations

--- src/main/java/com/spotifywebapp/app/DataController.java
+++ src/main/java/com/spotifywebapp/app/DataController.java
@@ -1,5 +1,9 @@
 package com.spotifywebapp.app;
 
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -9,12 +13,14 @@ import com.spotifywebapp.app.SpotifyWebAPI;
 import com.spotifywebapp.app.SpotifyWebAPISingleton;
 
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Controller
 public class DataController {
 
     public SpotifyWebAPI api = SpotifyWebAPISingleton.getInstance();
+    private MongoDBClient mongoClient = MongoDBSingleton.getInstance();
 
     @GetMapping("/datatransfer")
     public @ResponseBody String dataTransfer() {
@@ -23,6 +29,36 @@ public class DataController {
         HttpResponse response = handler.sendRequest();
         System.out.println(response.toString());
         return response.toString();
+    }
+
+    @GetMapping("/query_friend")
+    public @ResponseBody ResponseEntity<JSONObject> queryFriend(@RequestParam(name="id_query") String id_query) {
+
+        ArrayList<String> matchedPattern = mongoClient.findMatchingFriends(id_query);
+
+        JSONObject obj = new JSONObject();
+        obj.put("queries", matchedPattern.toArray());
+        return ResponseEntity.status(HttpStatus.OK).body(obj);
+    }
+
+    @PostMapping("/add_friend")
+    public @ResponseBody
+    ResponseEntity.BodyBuilder addFriend(@RequestBody Friend friend) {
+
+        String flag = mongoClient.addFriend(friend.getUser(), friend.getFriend());
+
+        return ResponseEntity.status(HttpStatus.OK).header("status", flag);
+
+    }
+
+    @DeleteMapping ("/remove_friend")
+    public @ResponseBody
+    ResponseEntity.BodyBuilder removeFriend(@RequestBody Friend friend) {
+
+        mongoClient.deleteFriend(friend.getUser(), friend.getFriend());
+
+        return ResponseEntity.status(HttpStatus.OK);
+
     }
 
 }
