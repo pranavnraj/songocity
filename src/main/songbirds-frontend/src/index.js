2c6e755231420e0d4869eb424ce8da577346e589
Cheery <cheeryone821@gmail.com>
2021-02-07 02:44:43 -0700

    set up global navbar login status

--- src/main/songbirds-frontend/src/index.js
+++ src/main/songbirds-frontend/src/index.js
@@ -17,44 +17,9 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import App from "views/App.js";
 
-import "assets/css/nucleo-icons.css";
-import "assets/scss/blk-design-system-react.scss?v=1.2.0";
-import "assets/demo/demo.css";
-
-import Index from "views/Index.js";
-import LandingPage from "views/examples/LandingPage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import SignInPage from "views/examples/SignInPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import Homepage from "views/examples/Homepage.js";
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/components" render={(props) => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        render={(props) => <LandingPage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={(props) => <RegisterPage {...props} />}
-      />
-      <Route
-        path="/sign-in-page"
-        render={(props) => <SignInPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={(props) => <ProfilePage {...props} />}
-      />
-      <Route
-        path="/homepage"
-        render={(props) => <Homepage {...props}/>}
-        />
-      <Redirect from="/" to="/components" />
-    </Switch>
-  </BrowserRouter>,
+  <App />,
   document.getElementById("root")
 );
