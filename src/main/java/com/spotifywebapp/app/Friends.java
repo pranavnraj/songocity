f799f8ba60a16d7e60bb711069554c0354cab5de
pranavnraj <pranavnraj@gmail.com>
2021-02-06 22:45:30 -0800

    Added Flask server for Machine Learning, as well as HTTP client to Spring Boot to connect Flask with Spring

---
+++ src/main/java/com/spotifywebapp/app/Friends.java
@@ -0,0 +1,25 @@
+package com.spotifywebapp.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Friends {
+
+    private List<String> friendIDs;
+
+    public Friends(){
+        super();
+    }
+
+    public Friends(List<String> friendIDs) {
+        this.friendIDs = friendIDs;
+    }
+
+    public void setFriendIDs(List<String> friendIDs) {
+        this.friendIDs = friendIDs;
+    }
+
+    public List<String> getFriendIDs() {
+        return friendIDs;
+    }
+}
