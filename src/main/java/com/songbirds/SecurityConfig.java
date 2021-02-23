98fdc655a9611ef4c5703a5328055d1266f2c070
pranavnraj <pranavnraj@gmail.com>
2021-02-23 02:51:25 -0800

    Fixed CSRF issue, Added authorize endpoint to browser client

--- src/main/java/com/songbirds/SecurityConfig.java
+++ src/main/java/com/songbirds/SecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
-        http.csrf().disable();
+        //http.csrf().disable();
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
