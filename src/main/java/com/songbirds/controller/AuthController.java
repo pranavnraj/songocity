ade24df9421d647681c32b3bef7920806d88bcce
pranavnraj <pranavnraj@gmail.com>
2021-03-01 23:51:12 -0800

    Full App Synchronization Implementing

--- src/main/java/com/songbirds/controller/AuthController.java
+++ src/main/java/com/songbirds/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.songbirds.controller;
 
 import com.songbirds.app.MongoDBClient;
 import com.songbirds.app.SpotifyWebAPI;
+import com.songbirds.objects.LoginThreadLock;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -20,30 +21,27 @@ public class AuthController {
 
     private SpotifyWebAPI api = SpotifyWebAPI.getInstance();
     private MongoDBClient mongoClient = MongoDBClient.getInstance();
-    private Object syncObject = new Object();
     private static final Logger LOGGER = Logger.getLogger(AuthController.class.getName());
-
-    private String currentID = "";
 
     @RequestMapping(value = "/login", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(origins="http://localhost:3000", allowCredentials = "true")
-    public ResponseEntity login(HttpSession session) {
-        api.initializeAPI();
+    public ResponseEntity login(@RequestParam(name="state") String state) {
 
-        synchronized (syncObject) {
+        LoginThreadLock currentLoginThreadLock = LoginThreadLock.getLoginLock(state);
+        if (currentLoginThreadLock == null) {
+            currentLoginThreadLock = new LoginThreadLock(state);
+            LoginThreadLock.addToLoginLocks(currentLoginThreadLock);
+        }
+
+        synchronized (currentLoginThreadLock) {
             try {
-                // Calling wait() will block this thread until another thread
-                // calls notify() on the object.
-                syncObject.wait();
+                currentLoginThreadLock.wait();
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
             }
         }
-        
-        HashMap<String, String> userInfo = api.currentUserAPI(currentID);
-        session.setAttribute("user_id", userInfo.get("id"));
-        LOGGER.log(Level.INFO, "User ID: " + session.getAttribute("user_id"));
 
+        LoginThreadLock.removeFromLoginLocks(currentLoginThreadLock);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
@@ -59,92 +57,35 @@ public class AuthController {
 
     @RequestMapping(value = "/callback", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(origins="http://localhost:3000")
-    public ResponseEntity<String> callback(@RequestParam(name="code") String code) {
+    public ResponseEntity<String> callback(@RequestParam(name="code") String code,
+                                           @RequestParam(name="state") String state, HttpSession session) {
         LOGGER.setLevel(Level.INFO);
 
-        System.out.println(code);
-
+        api.initializeAPI();
         String id = api.storeTokensUponLogin(code);
-        currentID = id;
-
-        synchronized (syncObject) {
-            syncObject.notify();
-        }
-
         HashMap<String, String> userInfo = api.currentUserAPI(id);
 
-        LOGGER.setLevel(Level.INFO);
-        System.out.println("userInfo.get(id): " + userInfo.get("id"));
-        if (mongoClient.getProfile(userInfo.get("id")) == null) {
-            mongoClient.createNewProfile(userInfo);
+        LoginThreadLock currentLoginThreadLock;
+        while(true) {
+            //System.out.println("In forever loop: " + state + "-----" + LoginThreadLock.loginThreadLocks.get(0).getStateID() + "-----" + LoginThreadLock.loginThreadLocks.get(1).getStateID());
+            currentLoginThreadLock = LoginThreadLock.getLoginLock(state);
+            if(currentLoginThreadLock != null) {
+                break;
+            }
         }
 
-        // Profile
-        System.out.println("Display Name: " + userInfo.get("display_name"));
-        System.out.println();
+        synchronized (currentLoginThreadLock) {
+            session.setAttribute("user_id", id);
+            if (mongoClient.getProfile(userInfo.get("id")) == null) {
+                mongoClient.createNewProfile(userInfo);
+            }
+            currentLoginThreadLock.notify();
+        }
 
         JSONObject obj = new JSONObject();
         obj.put("id", userInfo.get("id"));
         obj.put("display_name", userInfo.get("display_name"));
         obj.put("email", userInfo.get("email"));
-
-        /*
-        ArrayList<String> userRecentTracks = api.currentUserRecentTracks();
-        System.out.println("Recently Listened to: " + userRecentTracks);
-        System.out.println();
-        ArrayList<String> userTopArtists = api.currentUserTopArtists();
-        System.out.println("Your Top Artists... " + userTopArtists);
-        System.out.println();
-        ArrayList<String> userTopTracks = api.currentUserTopTracks();
-        System.out.println("Your Top Tracks... " + userTopTracks);
-        System.out.println();
-
-
-        // Creating Hashmap to be passed to python
-        int numTracks = 0;
-        System.out.println("Analyzing Playlists... ");
-        HashMap<String, HashMap<String, HashMap<String, Float>>> playlistsInfo = new HashMap<String, HashMap<String, HashMap<String, Float>>>();
-        HashMap<String, String> playlistInfo = api.currentUserPlaylists();
-        for(String playlistId: playlistInfo.keySet() ){
-            String playlistName = playlistInfo.get(playlistId);
-            playlistsInfo.put(playlistId, new HashMap<String, HashMap<String, Float>>());
-
-            System.out.println(playlistName + ": " + playlistId);
-            HashMap<String,String> playlistTracks = api.getTracks(playlistId);
-            numTracks += playlistTracks.keySet().size();
-            HashMap<String,HashMap<String,Float>> playlistTrackInfo = api.getTracksInfo(playlistTracks);
-            for(String trackId: playlistTrackInfo.keySet()){
-                String trackName = playlistTracks.get(trackId);
-                playlistsInfo.get(playlistId).put(trackId, new HashMap<String, Float>());
-                for(String feature : playlistTrackInfo.get(trackId).keySet()){
-                    playlistsInfo.get(playlistId).get(trackId).put(feature, playlistTrackInfo.get(trackId).get(feature));
-//                    System.out.println(feature + ": " + playlistTrackInfo.get(trackId).get(feature));
-                }
-            }
-
-        }
-
-        System.out.println("The Hashmap to be passed to Python");
-
-        for(String playlistName : playlistsInfo.keySet()) {
-//            System.out.println("playlist: " + playlistName);
-            for (String trackName : playlistsInfo.get(playlistName).keySet()) {
-//                System.out.println("track: " + trackName);
-//                for(String feature: playlistsInfo.get(playlistName).get(trackName).keySet()){
-//                    System.out.println(feature + ": " + playlistsInfo.get(playlistName).get(trackName).get(feature));
-//                }
-            }
-        }
-
-        // HttpClientHandler handler = new HttpClientHandler();
-        // handler.formRequest("http://localhost:8888/greeting");
-        // HttpResponse response = handler.sendRequest();
-        // System.out.println(response.toString());
-        // JSONObject obj = new JSONObject();
-        // obj.put("id", userInfo.get("id"));
-        // obj.put("display_name", userInfo.get("display_name"));
-        // return obj.toString();
-        */
 
         return ResponseEntity.status(HttpStatus.OK).body(obj.toString());
     }
