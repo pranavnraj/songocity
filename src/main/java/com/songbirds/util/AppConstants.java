d401257a376a8f7a48ef1d000df4f0241f31a3d8
pranavnraj <pranavnraj@gmail.com>
2021-03-06 16:44:17 -0800

    Threaded Training(fire n forget)

--- src/main/java/com/songbirds/util/AppConstants.java
+++ src/main/java/com/songbirds/util/AppConstants.java
@@ -3,4 +3,5 @@ package com.songbirds.util;
 public class AppConstants {
 
     public static final String FLASK_SERVER = "http://127.0.0.1:5000";
+    public static final int EXECUTOR_THREAD_COUNT = 10;
 }
