40163d7bde7844ceb15680e615534d5d70839eda
pranavnraj <pranavnraj@gmail.com>
2021-03-09 22:38:14 -0800

    Token Authentication Cookie filter implemented

--- src/main/java/com/songbirds/SecurityConfig.java
+++ src/main/java/com/songbirds/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
@@ -13,6 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
         //http.csrf().disable();
+        http.cors();
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+        http.addFilterBefore(new CheckAuthCookieFilter(), BasicAuthenticationFilter.class);
     }
 }
