4e40c3960d482dbbbc646d841293be50bfe99b29
pranavnraj <pranavnraj@gmail.com>
2021-03-10 21:11:16 -0800

    Refactored filter class

--- src/main/java/com/songbirds/SecurityConfig.java
+++ src/main/java/com/songbirds/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.songbirds;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import com.songbirds.filters.CheckAuthCookieFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
