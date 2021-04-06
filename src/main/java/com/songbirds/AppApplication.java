2029fc5dca0c6501376d0db26b4c04ef195b3737
pranavnraj <pranavnraj@gmail.com>
2021-04-05 19:55:39 -0700

    Package front end and back end together for deployment purposes

--- src/main/java/com/songbirds/AppApplication.java
+++ src/main/java/com/songbirds/AppApplication.java
@@ -3,6 +3,10 @@ package com.songbirds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AppApplication {
@@ -11,4 +15,14 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Bean
+	WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/blk-design-system-react/**")
+						.addResourceLocations("classpath:/blk-design-system-react/");
+			}
+		};
+	}
 }
