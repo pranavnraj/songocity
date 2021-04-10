88b63b20b69adf974f1e2a1d47b0c9a87a54f906
pranavnraj <pranavnraj@gmail.com>
2021-04-09 18:17:19 -0700

    Added OAuth Callback Page

--- src/main/java/com/songbirds/filters/CheckAuthCookieFilter.java
+++ src/main/java/com/songbirds/filters/CheckAuthCookieFilter.java
@@ -22,7 +22,7 @@ public class CheckAuthCookieFilter implements Filter {
     //private Logger logger = LoggerFactory.getLogger(getClass());
 
     private MongoDBClient mongoClient = MongoDBClient.getInstance();
-    private List<String> unauthenticatedRoutes = new ArrayList<String>(Arrays.asList("/login", "/callback/", "/prime_login", "/", "/manifest.json", "/favicon.ico"));
+    private List<String> unauthenticatedRoutes = new ArrayList<String>(Arrays.asList("/login", "/callback/", "/prime_login", "/", "/friends", "/manifest.json", "/favicon.ico", "/profile-page", "/sign-in-page", "/register-page", "/landing-page"));
     private static final Logger LOGGER = Logger.getLogger(CheckAuthCookieFilter.class.getName());
 
     @Override
