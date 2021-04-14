d45189dd1be83428d397a5cd6806d11e497fad84
pranavnraj <pranavnraj@gmail.com>
2021-04-13 17:19:35 -0700

    Change callback when deploying

--- src/main/java/com/songbirds/util/LoginCredentialConstants.java
+++ src/main/java/com/songbirds/util/LoginCredentialConstants.java
@@ -5,4 +5,5 @@ public class LoginCredentialConstants {
     public static final String CLIENT_ID = "3cceff6ff3144834b845505bcfab9cd7";
     public static final String CLIENT_SECRET = "44962f5861be4acc808ac5f2fe1c28fd";
     public static final String REDIRECT_URI = "http://localhost:8888/callback/";
+    //public static final String REDIRECT_URI = DNS NAME;
 }
