3672eedb7ee79c4bcba4cfce9f308661c72e4180
pranavnraj <pranavnraj@gmail.com>
2021-04-20 22:18:57 -0700

    Fixed recommender page errors

--- src/main/java/com/songbirds/util/AppConstants.java
+++ src/main/java/com/songbirds/util/AppConstants.java
@@ -2,7 +2,7 @@ package com.songbirds.util;
 
 public class AppConstants {
 
-    public static final String FLASK_SERVER = "http://127.0.0.1:5000";
+    public static final String FLASK_SERVER = "http://127.0.0.1:5001";
     public static final int EXECUTOR_THREAD_COUNT = 10;
     public static final long MILLISECONDS_IN_YEAR = 31556952000L;
 }
