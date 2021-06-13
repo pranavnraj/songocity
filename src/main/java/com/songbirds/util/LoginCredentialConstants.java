756c1358dab94cb3af2ab8282244489844241013
pranavnraj <pranavnraj@gmail.com>
2021-06-13 01:44:05 -0700

    Wrap up

---
+++ src/main/java/com/songbirds/util/LoginCredentialConstants.java
@@ -0,0 +1,9 @@
+package com.songbirds.util;
+
+public class LoginCredentialConstants {
+
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
+    // public static final String REDIRECT_URI = "http://localhost:5000/callback/";
+    public static final String REDIRECT_URI = "http://songocity.com/callback/";
+}
